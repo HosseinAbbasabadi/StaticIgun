cf1d88388febf30742176f48554f237931edc5d3
Hossein Abbasabadi <Hossein.Abbasabadi@outlook.com>
2019-12-30 18:13:43 +0330

    init

---
+++ code/publish/admin/wwwroot/fileManager/fileManager.js
@@ -0,0 +1,150 @@
+﻿const host = "https://localhost:5001";
+const fileManagerApiEndpoint = "https://localhost:5001/api/FileManagere/";
+
+function getFiles(path) {
+  get(`${fileManagerApiEndpoint}ListFiles`,
+    "PUT",
+    {
+      hostName: host,
+      path: path,
+      searchPattern: ""
+    },
+    "file",
+    "#listoffiles");
+}
+
+function getDirectories(path) {
+  get(`${fileManagerApiEndpoint}ListDirectories`,
+    "PUT",
+    {
+      hostName: host,
+      path: path,
+      searchPattern: ""
+    },
+    "directory",
+    "#listofdirectories");
+}
+
+function get(url, httpMethod, data, type, fileContainerDiv) {
+  $.ajax(url,
+    {
+      type: httpMethod,
+      dataType: "json",
+      contentType: "application/json; charset=utf-8",
+      data: JSON.stringify(data)
+    }).done(function(result) {
+    handleGetCallBack(result, type, fileContainerDiv);
+  }).fail(function(error) {
+    alert(error);
+  });
+}
+
+function handleGetCallBack(data, type, fileContainerDiv) {
+  var result = '';
+
+  switch (type) {
+  case "file":
+    data.forEach(function(item, index) {
+      const fileDiv =
+        `<div class='col-md-2 m-2 file-item' id="selectable-file-item"><img src=${item
+          } class="rounded img-fluid"/></div>`;
+      result += fileDiv;
+    });
+    break;
+  case "directory":
+    data.forEach(function(item, index) {
+      const fileDiv =
+        `<div class='col-md-2 text-center m-2 file-item' onclick="routeToDirectory('${item.path
+          }')"><img src='/fileManager/folder-icon.png' class="rounded img-fluid" /><span>${item.name}</span></div>`;
+      result += fileDiv;
+    });
+    break;
+  default:
+  }
+
+  $(fileContainerDiv).html(result);
+
+  const targetInput = localStorage.getItem("targetInput");
+  if (targetInput === undefined)
+    return;
+  $("#selectable-file-item img").click(function() {
+    const value = $(this).attr("src");
+    $(`#${targetInput}`).val(value);
+  });
+}
+
+function OpenFileUploader(targetInput) {
+  localStorage.setItem("targetInput", targetInput);
+  localStorage.setItem("currentPath", "");
+
+  $("#fileManagerModal").modal("show");
+  getFiles("", targetInput);
+  getDirectories("");
+}
+
+function routeToDirectory(directoryName) {
+  localStorage.setItem("currentPath", directoryName);
+  getFiles(directoryName);
+  getDirectories(directoryName);
+  //updateBackBottun();
+}
+
+function updateBackBottun() {
+  var prevDirectory = localStorage.getItem("prevDirectory");
+  if (prevDirectory === "")
+    prevDirectory = "root";
+  const backBtn = `<button type="button" onclick="routeToDirectory('')">${prevDirectory}</button>`;
+  $("#filemanager-backBtn-breadcrump").html(backBtn);
+}
+
+function upload() {
+  const input = document.getElementById("fileManager-upload-file");
+  const files = input.files;
+  const formData = new FormData();
+  for (let i = 0; i !== files.length; i++) {
+    formData.append("files", files[i]);
+  }
+
+  const path = localStorage.getItem("currentPath");
+  $.ajax(
+    {
+      url: `${fileManagerApiEndpoint}Upload?path=${path}`,
+      data: formData,
+      processData: false,
+      contentType: false,
+      type: "POST",
+      success: function () {
+        routeToDirectory(path);
+        //if (filename === 400) {
+
+        //}
+        //sendNotification("error", "top-right", "خطا", "فایل نامعتبر است. لطفا دوباره تلاش کنید");
+        //if (filename === 401) {
+
+        //}
+        //sendNotification("error", "top-right", "خطا", "حجم عکس باید کمتر از ۳ مگابایت باشد");
+        //else {
+        //sendNotification("success", "top-right", "موفیت", "عکس با موفقیت بارگذاری شد")
+        //$('#' + deleteBtnId).removeClass("hidden");
+        //$('#' + imagePropertyId).attr("value", filename);
+        //}
+      }
+    });
+}
+
+function createDirectory(formId) {
+  const path = localStorage.getItem("currentPath");
+  const sendingData = $(`#${formId}`).serialize();
+
+  $.ajax(
+    {
+      url: `${fileManagerApiEndpoint}CreateDirectory?path=${path}`,
+      data: JSON.stringify(sendingData),
+      dataType: "json",
+      contentType: "application/json; charset=utf-8",
+      type: "POST",
+      success: function () {
+        routeToDirectory(path);
+      }
+    });
+}
---
+++ code/publish/admin/wwwroot/js/site.js
@@ -0,0 +1,58 @@
+﻿function makeSlug(source, dist, url) {
+  const takedata = $('#' + source).val();
+  $('#' + dist).val(convertToSlug(takedata));
+  checkSlugDuplication(url, dist);
+}
+
+var convertToSlug = function(str) {
+  var $slug = '';
+  const trimmed = $.trim(str);
+  $slug = trimmed.replace(/[^a-z0-9-آ-ی-]/gi, '-').replace(/-+/g, '-').replace(/^-|-$/g, '');
+  return $slug.toLowerCase();
+};
+
+function checkSlugDuplication(url, dist) {
+  const slug = $('#' + dist).val();
+  const id = convertToSlug(slug);
+  $.get({
+    url: url + '/' + id,
+    contentType: "application/json; charset=utf-8",
+    success: function(result) {
+      if (result === true) {
+        $('#duplicated-slug-error').removeClass("d-none");
+      }
+    }
+  });
+}
+
+var count = 2;
+
+function dynamicElement(parentElement) {
+  const division = document.createElement("DIV");
+  division.classList.add("form-group");
+  division.innerHTML = dynamicCardBox(this.count);
+  const prevCounter = this.count - 1;
+  if (document.getElementById(`removeBtn${prevCounter}`) !== null) {
+    document.getElementById(`removeBtn${prevCounter}`).classList.add("hidden");
+  }
+  document.getElementById(parentElement).appendChild(division);
+  this.count++;
+}
+
+function dynamicCardBox(count) {
+  return `<div class="input text">
+              <input type="text" name="Card${count}" id="Card${count}" class="single-input" maxlength="16"/>
+              <br />
+              <input class="btn btn-danger btn-sm" type="button" value="حذف" onclick="removeCardBox(this)" id="removeBtn${
+    count}"/>
+            </div>
+        `;
+}
+
+function removeCardBox(div) {
+  document.getElementById("bankCards").removeChild(div.parentNode.parentNode);
+  this.count--;
+  if (document.getElementById(`removeBtn${this.count - 1}`) !== null) {
+    document.getElementById(`removeBtn${this.count - 1}`).classList.remove("hidden");
+  }
+}
